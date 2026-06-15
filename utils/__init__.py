f7432166c07bc9afd165c4d1012551bc2ead676d
Idin Iskandar <167355867+idincodingweb@users.noreply.github.com>
2026-06-16 03:55:26 +0700

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+# Quick scaffold
+touch engines/__init__.py crawlers/__init__.py extractors/__init__.py \
+      database/__init__.py exporters/__init__.py utils/__init__.py \
+      tests/__init__.py outputs/.gitkeep
